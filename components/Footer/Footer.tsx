3c196cadea1eab5185934604b03596dcee1a2115
Eshaan Yadav <eshaanyadav24@gmail.com>
2023-04-22 11:52:30 +0530

    footer updates

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export const Footer = () => {
@@ -5,15 +6,24 @@ export const Footer = () => {
     <footer className="bg-black py-4 h-32 flex flex-col gap-6 justify-between items-center text-white">
       {
         <ul className="flex gap-14 w-fit m-auto">
-          <li className="hover:text-tone-1 tranform duration-200 text-white cursor-pointer">
-            Github
-          </li>
-          <li className="hover:text-tone-2 tranform duration-200 text-white cursor-pointer">
-            Twitter
-          </li>
-          <li className="hover:text-tone-1 tranform duration-200 text-white cursor-pointer">
-            LinkedIn
-          </li>
+          <Link href={"https://github.com/Eshaan-Y24"} target="_blank">
+            <li className="hover:text-tone-1 tranform duration-200 text-white cursor-pointer">
+              Github
+            </li>
+          </Link>
+          <Link href={"https://twitter.com/Eshaaaaann"} target="_blank">
+            <li className="hover:text-tone-2 tranform duration-200 text-white cursor-pointer">
+              Twitter
+            </li>
+          </Link>
+          <Link
+            href={"https://www.linkedin.com/in/eshaan-yadav-22b94b1b4/"}
+            target="_blank"
+          >
+            <li className="hover:text-tone-1 tranform duration-200 text-white cursor-pointer">
+              LinkedIn
+            </li>
+          </Link>
         </ul>
       }
       © Eshaan 2022
