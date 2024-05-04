f6a98990f22015a36f5274c9faa9a2435ba06bb7
e-for-eshaan <eshaanyadav24@gmail.com>
2024-05-04 21:51:53 +0530

    sets up redux store and stores user-obj on login

--- components/Auth/GoogleAuth.tsx
+++ components/Auth/GoogleAuth.tsx
@@ -1,15 +1,19 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import app from '../../firebaseConfig';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../redux/store';
+import { login } from '../../redux/authSlice';
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 const GoogleAuth: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
   const handleSignIn = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
-      console.log(result.user);
-    } catch (error:any) {
+      dispatch(login({ user: result.user }));
+    } catch (error: any) {
       console.error(error.message);
     }
   };
