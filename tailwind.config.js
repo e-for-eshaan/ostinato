cef77343834a9a32b75c78bba197e39b8963ca06
e-for-eshaan <eshaanyadav24@gmail.com>
2024-05-05 14:41:53 +0530

    styles formatter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,43 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./features/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './features/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
         righteous: ['Righteous', 'sans-serif'],
-
       },
       colors: {
-        'primary': '#000000',
-        'secondary': '#EAEAEA',
-        'tertiary': '#252A34',
-        'tone': {
+        primary: '#000000',
+        secondary: '#EAEAEA',
+        tertiary: '#252A34',
+        tone: {
           1: '#08D9D6',
           2: '#FF2E63',
-        }
+        },
       },
       keyframes: {
         appear: {
           '0%': { width: 0 },
           '100%': { width: '150px' },
-        }, disappear: {
+        },
+        disappear: {
           '0%': { width: '150px' },
           '100%': { width: 0 },
-        }
-        , spinner: {
+        },
+        spinner: {
           '0%': { width: '150px' },
           '100%': { width: 0 },
-        }
+        },
       },
       animation: {
         appear: 'appear 0.1s ease-in 1',
         'disappear-anim': 'appear 0.1s ease-in 1 reverse',
-      }
+      },
     },
   },
   plugins: [],
-}
+};
