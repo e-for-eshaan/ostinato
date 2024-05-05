f4c7b452952764a14f5a3be918e2eb7f52517c58
e-for-eshaan <eshaanyadav24@gmail.com>
2024-05-05 14:35:07 +0530

    updated reducer

--- redux/timeSlice.ts
+++ redux/timeSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { LoopType } from "../features/VideoPlayer/VideoPlayer";
 
-interface TimeState {
+export interface TimeState {
     currentTimeStamp: LoopType,
     allTimeStamps: LoopType[],
     timeMapById: Record<string, LoopType>,
@@ -19,8 +19,8 @@ const timeSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-        setCurrentTimeStamp: (state, action: PayloadAction<LoopType>) => {
-            state.currentTimeStamp = action.payload;
+        setCurrentTimeStamp: (state, action: PayloadAction<string | undefined>) => {
+            state.currentTimeStamp = state.timeMapById[action.payload as string] || {} as LoopType;
         },
         setAllTimeStamps: (state, action: PayloadAction<LoopType[]>) => {
             state.allTimeStamps = action.payload;
