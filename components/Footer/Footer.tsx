1443afa8fae4d5a086ec137aa6e4292e9763ef33
e-for-eshaan <eshaanyadav24@gmail.com>
2024-05-05 13:30:01 +0530

    Update links in Footer component

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,18 +6,18 @@ export const Footer = () => {
     <footer className="bg-black py-4 h-32 flex flex-col gap-6 justify-between items-center text-white">
       {
         <ul className="flex gap-14 w-fit m-auto">
-          <Link href={"https://github.com/Eshaan-Y24"} target="_blank">
+          <Link href={"https://github.com/e-for-eshaan/"} target="_blank">
             <li className="hover:text-tone-1 tranform duration-200 text-white cursor-pointer">
               Github
             </li>
           </Link>
-          <Link href={"https://twitter.com/Eshaaaaann"} target="_blank">
+          <Link href={"https://twitter.com/e_for_eshaan"} target="_blank">
             <li className="hover:text-tone-2 tranform duration-200 text-white cursor-pointer">
               Twitter
             </li>
           </Link>
           <Link
-            href={"https://www.linkedin.com/in/eshaan-yadav-22b94b1b4/"}
+            href={"https://linkedin.com/in/e-for-eshaan"}
             target="_blank"
           >
             <li className="hover:text-tone-1 tranform duration-200 text-white cursor-pointer">
