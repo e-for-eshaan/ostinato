305a53af0772f0ffa052c0a27fe4dd7aecf969fe
e-for-eshaan <eshaanyadav24@gmail.com>
2024-10-20 10:05:00 +0530

    fixes incorrect piano notes, adds type definition for ts, adds vs-code and eslint configs

--- components/Piano/Piano.tsx
+++ components/Piano/Piano.tsx
@@ -1,28 +1,18 @@
 import useSound from 'use-sound';
 
-//@ts-ignore
-import C from './1.mp3';
-//@ts-ignore
-import Cs from './2.mp3';
-//@ts-ignore
-import D from './3.mp3';
-//@ts-ignore
-import Ds from './4.mp3';
-//@ts-ignore
-import E from './5.mp3';
-//@ts-ignore
-import F from './6.mp3';
-//@ts-ignore
-import Fs from './7.mp3';
-//@ts-ignore
-import G from './8.mp3';
-//@ts-ignore
-import Gs from './9.mp3';
-//@ts-ignore
-import A from './10.mp3';
-//@ts-ignore
-import As from './11.mp3';
-//@ts-ignore
+import C from '../../public/notes2/C4.mp3';
+import Cs from '../../public/notes2/Db4.mp3';
+import D from '../../public/notes2/D4.mp3';
+import Ds from '../../public/notes2/Eb4.mp3';
+import E from '../../public/notes2/E4.mp3';
+import F from '../../public/notes2/F4.mp3';
+import Fs from '../../public/notes2/Gb4.mp3';
+import G from '../../public/notes2/G4.mp3';
+import Gs from '../../public/notes2/Ab4.mp3';
+import A from '../../public/notes2/A4.mp3';
+import As from '../../public/notes2/Bb4.mp3';
+import B from '../../public/notes2/B4.mp3';
+
 
 export const Piano = () => {
   const [playC] = useSound(C);
@@ -36,88 +26,54 @@ export const Piano = () => {
   const [playGs] = useSound(Gs);
   const [playA] = useSound(A);
   const [playAs] = useSound(As);
-  const clicker = (key: number) => {
-    switch (key) {
-      case 1: {
-        playC();
-        break;
-      }
-      case 2: {
-        playCs();
-        break;
-      }
-      case 3: {
-        playD();
-        break;
-      }
-      case 4: {
-        playDs();
-        break;
-      }
-      case 5: {
-        playE();
-        break;
-      }
-      case 6: {
-        playF();
-        break;
-      }
-      case 7: {
-        playFs();
-        break;
-      }
-      case 8: {
-        playG();
-        break;
-      }
-      case 9: {
-        playGs();
-        break;
-      }
-      case 10: {
-        playA();
-        break;
-      }
-      case 11: {
-        playAs();
-        break;
-      }
-      default: {
-        break;
-      }
-    }
-  };
+  const [playB] = useSound(B);
+
+  const playMap = [
+    playC,
+    playCs,
+    playD,
+    playDs,
+    playE,
+    playF,
+    playFs,
+    playG,
+    playGs,
+    playA,
+    playAs,
+    playB
+  ]
+
   return (
     <div className="flex relative bg-primary m-2">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(item => {
-        switch (item) {
+      {playMap.map((playFunc, index) => {
+        switch (index + 1) {
           case 2: {
             return (
-              <BlackKeys key={item} keyNum={item} clicker={clicker} className={'left-[35px]'} />
+              <BlackKeys key={index} play={playFunc} className={'left-[35px]'} />
             );
           }
-          case 3: {
+          case 4: {
             return (
-              <BlackKeys key={item} keyNum={item} clicker={clicker} className={'left-[91px]'} />
+              <BlackKeys key={index} play={playFunc} className={'left-[91px]'} />
             );
           }
           case 7: {
             return (
-              <BlackKeys key={item} keyNum={item} clicker={clicker} className={'left-[203px]'} />
+              <BlackKeys key={index} play={playFunc} className={'left-[203px]'} />
             );
           }
           case 9: {
             return (
-              <BlackKeys key={item} keyNum={item} clicker={clicker} className={'left-[259px]'} />
+              <BlackKeys key={index} play={playFunc} className={'left-[259px]'} />
             );
           }
           case 11: {
             return (
-              <BlackKeys key={item} keyNum={item} clicker={clicker} className={'left-[315px]'} />
+              <BlackKeys key={index} play={playFunc} className={'left-[315px]'} />
             );
           }
           default: {
-            return <WhiteKeys key={item} keyNum={item} clicker={clicker} />;
+            return <WhiteKeys key={index} play={playFunc} isLast={index === playMap.length - 1} />;
           }
         }
       })}
@@ -127,28 +83,23 @@ export const Piano = () => {
 
 interface Keys {
   className?: string;
-  clicker: (e: number) => void;
-  keyNum: number;
+  play: () => void;
 }
 
-const BlackKeys: React.FC<Keys> = ({ className, clicker, keyNum }) => {
+const BlackKeys: React.FC<Keys> = ({ className, play }) => {
   return (
     <div
-      onClick={() => {
-        clicker(keyNum);
-      }}
-      className={`${className} z-10 cursor-pointer hover:bg-tone-1 transform duration-100 w-10 h-40 inline-block bg-black absolute`}
+      onClick={play}
+      className={`${className} z-10 cursor-pointer text-white hover:bg-tone-1 transform duration-100 w-10 h-40 inline-block bg-black absolute`}
     />
   );
 };
 
-const WhiteKeys: React.FC<Keys> = ({ clicker, keyNum }) => {
+const WhiteKeys: React.FC<Keys & { isLast?: boolean }> = ({ play, isLast }) => {
   return (
     <div
-      onClick={() => {
-        clicker(keyNum);
-      }}
-      className={`hover:bg-tone-2 ${keyNum === 12 ? 'hidden sm:inline-block' : ''} cursor-pointer hover:skew-y-5 transform duration-100 w-12 h-56 inline-block mx-1 bg-white`}
+      onClick={play}
+      className={`hover:bg-tone-2 flex  flex-col-reverse ${isLast ? 'hidden sm:inline-block' : ''} cursor-pointer hover:skew-y-5 transform duration-100 w-12 h-56 inline-block mx-1 bg-white`}
     />
   );
 };
--- redux/index.ts
+++ redux/index.ts
@@ -1,5 +1,9 @@
-import { useSelector as useReactSelector, useDispatch as useReactDispatch } from 'react-redux';
+import {
+  useSelector as useReactSelector,
+  useDispatch as useReactDispatch,
+  TypedUseSelectorHook,
+} from 'react-redux';
 import { RootState, RootDispatch } from './store';
 
-export const useSelector = useReactSelector<RootState>;
-export const useDispatch = useReactDispatch<RootDispatch>;
+export const useSelector: TypedUseSelectorHook<RootState> = useReactSelector;
+export const useDispatch = () => useReactDispatch<RootDispatch>();
