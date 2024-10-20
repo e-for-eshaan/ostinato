bd97ef84c7e1ff040e84fce4eeb10f15c02c88a3
e-for-eshaan <eshaanyadav24@gmail.com>
2024-10-20 12:32:37 +0530

    fixes loading states

--- components/Auth/GoogleAuth.tsx
+++ components/Auth/GoogleAuth.tsx
@@ -2,8 +2,9 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { app, auth } from '../../firebaseConfig';
 import { useDispatch } from 'react-redux';
 import { useSelector } from '../../redux';
-import { storeJWT } from '../../utils/functions';
+import { storeJWT, getJWT } from '../../utils/functions';
 import { authSlice } from '../../redux/authSlice';
+import { useState, useCallback, useEffect } from 'react';
 
 const provider = new GoogleAuthProvider();
 
@@ -11,8 +12,10 @@ const GoogleAuth: React.FC = () => {
   const dispatch = useDispatch();
   const isSignedIn = useSelector(state => state.auth.isLoggedIn);
   const provider = new GoogleAuthProvider();
+  const [signInLoading, setSignInLoading] = useState(false);
 
   const handleSignIn = async () => {
+    setSignInLoading(true);
     try {
       const result = await signInWithPopup(auth, provider)
         .then(async (res) => {
@@ -36,16 +39,96 @@ const GoogleAuth: React.FC = () => {
     } catch (error: any) {
       console.error(error.message);
     }
+    setSignInLoading(false);
+
   };
+
   const handleSignOut = () => {
     dispatch(authSlice.actions.logout());
   };
 
+  const fetchUserData = useCallback(async (token: string) => {
+    setSignInLoading(true);
+    const response = await fetch("/api/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ token }),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      dispatch(authSlice.actions.login({ user: data.user }));
+      console.log("User data fetched successfully:", data.user);
+    } else {
+      console.error("Failed to fetch user data:", data.error);
+    }
+    setSignInLoading(false);
+  }, [])
+
+  const handleFirstLogin = useCallback(async () => {
+    setSignInLoading(true);
+    try {
+      const token = getJWT();
+      if (!token) {
+        setSignInLoading(false);
+        return
+      }
+      await fetchUserData(token);
+    }
+    catch (error: any) {
+      console.error(error.message);
+    }
+    setSignInLoading(false);
+  }, []);
+
+  useEffect(() => {
+    handleFirstLogin()
+  }, [])
+
   return (
     <span key={String(isSignedIn)} onClick={isSignedIn ? handleSignOut : handleSignIn}>
-      {isSignedIn ? 'Sign out' : 'Sign in'}
+      {
+        signInLoading ?
+          <Loader size={20} />
+          : isSignedIn
+            ? 'Sign out'
+            : 'Sign in'
+      }
     </span>
   );
 };
 
 export default GoogleAuth;
+
+type LoaderProps = {
+  size?: number; // Size of the loader in pixels
+  color?: string; // Color of the loader
+}
+
+const Loader: React.FC<LoaderProps> = ({ size = 80, color = "white" }) => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <svg
+        className="animate-spin"
+        width={size}
+        height={size}
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="50"
+          cy="50"
+          r="45"
+          stroke={color}
+          strokeWidth="5"
+          fill="none"
+          strokeLinecap="round"
+          strokeDasharray="283" // 2 * π * 45 (circumference of the circle)
+          strokeDashoffset="75" // Adjust this value for animation
+        />
+      </svg>
+    </div>
+  );
+};
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -11,42 +11,6 @@ interface LayoutProps {
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const dispatch = useDispatch();
-
-  const fetchUserData = useCallback(async (token: string) => {
-    const response = await fetch("/api/auth", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ token }),
-    });
-
-    const data = await response.json();
-    if (response.ok) {
-      dispatch(authSlice.actions.login({ user: data.user }));
-      console.log("User data fetched successfully:", data.user);
-    } else {
-      console.error("Failed to fetch user data:", data.error);
-    }
-  }, [])
-
-  const handleFirstLogin = useCallback(async () => {
-    try {
-      const token = getJWT();
-      if (!token) {
-        return
-      }
-      await fetchUserData(token);
-    }
-    catch (error: any) {
-      console.error(error.message);
-    }
-  }, []);
-
-  useEffect(() => {
-    handleFirstLogin()
-  }, [])
   return (
     <main className="min-h-screen">
       <Navbar />
